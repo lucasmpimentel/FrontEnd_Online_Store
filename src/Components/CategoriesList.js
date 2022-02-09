b2851888ce9a1aeb88bc8f4ef197119826173331
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-02-09 16:58:18 -0300

    Requisito 4

--- src/Components/CategoriesList.js
+++ src/Components/CategoriesList.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class CategoriesList extends Component {
   render() {
     const { categorieName } = this.props;
     return (
-        <div>
-          <button type="button" data-testid="category">{categorieName}</button>
-        </div>
-    )
+      <div>
+        <button type="button" data-testid="category">{categorieName}</button>
+      </div>
+    );
+  }
 }
-}
+
+CategoriesList.propTypes = {
+  categorieName: PropTypes.string.isRequired,
+};
 
 export default CategoriesList;
