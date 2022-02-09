854893c95edb4c52930a5821a823abf577f14f95
Martin <martinpereztovar@gmail.com>
2022-02-09 16:46:47 -0300

    requisito 3

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class MainPage extends Component {
   render() {
     return (
       <div>
         <h1>Página Inicial</h1>
+        <Link
+          to="/cart"
+          data-testid="shopping-cart-button"
+        >
+          Carrinho de compras
+        </Link>
         <input type="text" />
         <div>
           {/* <span>Produtos aqui</span> */}
