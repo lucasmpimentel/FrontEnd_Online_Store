a4951f85352d59cadbc3d274bb54964f568507a6
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-02-09 16:47:30 -0300

    -Main Group 1 - Requisito 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import MainPage from './pages/MainPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ MainPage } />
+      </BrowserRouter>
+    );
+  }
 }
+
+App.propTypes = {
+
+};
 
 export default App;
