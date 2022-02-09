45a63d1a5b8e982869ad102f29f399e573a9dd1c
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-02-09 18:03:25 -0300

    main-group-1-req-5

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes, { oneOfType } from 'prop-types';
 
 export default class CardProducts extends Component {
   render() {
+    const { product: { title, price, thumbnail } } = this.props;
+    console.log(this.props);
     return (
       <div data-testid="product">
-        CardProducts
+        <p>{ title }</p>
+        <img src={ thumbnail } alt={ title } />
+        <p>R$ { price } </p>
       </div>
     );
   }
+}
+
+CardProducts.propTypes={
+  product: PropTypes.objectOf(oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ])).isRequired
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,53 +1,77 @@
 import React, { Component } from 'react';
 import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import Header from '../components/Header';
-import Loading from '../components/Loading'
-import CategoriesList from '../Components/CategoriesList';
+import Loading from '../components/Loading';
+import CategoriesList from '../components/CategoriesList';
+import CardProducts from '../components/CardProducts';
+import '../styles/MainPage.css';
 
-class MainPage extends Component {
+export default class MainPage extends Component {
   state = {
     loading: false,
     loaded: false,
     search: '',
     listCategories: [],
+    listProducts: [],
+  }
+
+  async componentDidMount() {
+    const list = await getCategories();
+    this.setState({ listCategories: list });
   }
 
   handleInputChange = ({ target }) => {
     const { name, value } = target;
     this.setState({ [name]: value });
-    // getProductsFromCategoryAndQuery(categoryId, query)
   }
 
-  async componentDidMount() {
-    const list = await getCategories();
-    this.setState({listCategories: list});
-    console.log(list);
+  handleClick = () => {
+    const { search } = this.state;
+    const categoryId = '';
+    this.setState({ loading: true }, async () => {
+      const getProducts = await getProductsFromCategoryAndQuery(categoryId, search);
+      this.setState({ listProducts: getProducts, loading: false, loaded: true });
+    });
   }
 
   render() {
-    const {loading, loaded, search, listCategories} = this.state;
+    const { loading, loaded, search, listCategories, listProducts } = this.state;
     return (
       <div>
         <Header />
-        <input data-testid="query-input" name="search" type="text" value={ search } onChange={ this.handleInputChange } />
-        <button data-testid="query-button">Pesquisar</button>
-        <div>
-          { loading && <Loading />
-            // aqui entra o map pra renderizar o card de produtos 
-          }
-          <p data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
+        <input
+          data-testid="query-input"
+          name="search"
+          type="text"
+          value={ search }
+          onChange={ this.handleInputChange }
+        />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.handleClick }
+        >
+          Pesquisar
+        </button>
+        <div className="categories-praducts">
+          <section>
+            { listCategories.map((item) => (
+              <CategoriesList
+                key={ item.id }
+                categorieName={ item.name }
+              />))}
+          </section>
+          <div>
+            { loading && <Loading /> }
+            { loaded && listProducts.results.map((product) => (
+              <CardProducts key={ product.id } product={ product } />
+            ))}
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          </div>
         </div>
-        <section>
-          {listCategories.map((item) => <CategoriesList key={item.id} categorieName={item.name} />)
-          }
-        </section>
       </div>
     );
   }
 }
-
-MainPage.propTypes = {};
-
-export default MainPage;
