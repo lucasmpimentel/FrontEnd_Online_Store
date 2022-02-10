31c55e8fef948de5e80e9d7fd7a0823fce038eec
Lucas Lisboa <lucas@pop-os.localdomain>
2022-02-10 16:45:10 -0300

    main group 1, requisito 8

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes, { oneOfType } from 'prop-types';
 import '../styles/CardProducts.css';
+import { addCart } from '../services/addCart';
 
 export default class CardProducts extends Component {
   render() {
+    const { product } = this.props;
     const { product: { title, price, thumbnail } } = this.props;
     console.log(this.props);
     return (
       <div data-testid="product" className="card-product-container">
         <h4 className="title-card-product">{ title }</h4>
         <img className="img-card-product" src={ thumbnail } alt={ title } />
-        <p>
-          {`R$ ${price}`}
-        </p>
+        <p>{`R$ ${price}`}</p>
+        <button
+          data-testid="product-add-to-cart"
+          type="button"
+          onClick={ () => addCart(product) }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { getCart } from '../services/addCart';
+import CartProductCard from '../components/CartProductCard';
 
 class Cart extends Component {
+  state = {
+    cart: [],
+  }
+
+  componentDidMount() {
+    const cart = getCart();
+    if (cart) this.setState({ cart });
+  }
+
   render() {
+    const { cart } = this.state;
     return (
       <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        {cart.length === 0
+        && <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
+        {cart.length !== 0
+        && Object.values(cart.reduce((acc, product) => {
+          const includesId = Object.keys(acc).some((id) => id === product.id);
+          if (includesId) acc[product.id] = [product, (acc[product.id][1] + 1)];
+          else acc[product.id] = [product, 1];
+          return acc;
+        }, {})).map(([product, amount]) => (
+          <CartProductCard
+            key={ product.id }
+            product={ product }
+            amount={ amount }
+          />
+        ))}
       </div>
     );
   }
