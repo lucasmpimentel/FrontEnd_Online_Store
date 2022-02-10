a7cce565e9bc5e88b878112d4869341b5aa57e97
Lucas Lisboa <lucas@pop-os.localdomain>
2022-02-10 17:01:57 -0300

    merge pull

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -19,15 +19,28 @@ export default class CardProducts extends Component {
   };
 
   render() {
-<<<<<<< HEAD
     const { product } = this.props;
-    const { product: { title, price, thumbnail } } = this.props;
-    console.log(this.props);
+    const { title, price, thumbnail, id } = product;
     return (
-      <div data-testid="product" className="card-product-container">
-        <h4 className="title-card-product">{ title }</h4>
-        <img className="img-card-product" src={ thumbnail } alt={ title } />
-        <p>{`R$ ${price}`}</p>
+      // Tentando passar o resultado de pesquisa que gerou o Card para a próxima página (ProducDetails)
+      // https://stackoverflow.com/questions/45598854/passing-values-through-react-router-v4-link
+      <div
+        data-testid="product"
+        className="card-product-container"
+        props={ (title, price, thumbnail, id) }
+      >
+        <Link
+          data-testid="product-detail-link"
+          to={ {
+            pathname: `/productDetail/${id}`,
+          } }
+          onClick={ this.saveToLocalStorage }
+        >
+          <h4 className="title-card-product">{title}</h4>
+          <img className="img-card-product" src={ thumbnail } alt={ title } />
+          <p>{`R$ ${price}`}</p>
+        </Link>
+        <hr />
         <button
           data-testid="product-add-to-cart"
           type="button"
@@ -36,31 +49,6 @@ export default class CardProducts extends Component {
           Adicionar ao carrinho
         </button>
       </div>
-=======
-    const {
-      product: { title, price, thumbnail, id },
-    } = this.props;
-    return (
-      // Tentando passar o resultado de pesquisa que gerou o Card para a próxima página (ProducDetails)
-      // https://stackoverflow.com/questions/45598854/passing-values-through-react-router-v4-link
-      <Link
-        data-testid="product-detail-link"
-        to={ {
-          pathname: `/productDetail/${id}`,
-        } }
-        onClick={ this.saveToLocalStorage }
-      >
-        <div
-          data-testid="product"
-          className="card-product-container"
-          props={ (title, price, thumbnail, id) }
-        >
-          <h4 className="title-card-product">{title}</h4>
-          <img className="img-card-product" src={ thumbnail } alt={ title } />
-          <p>{`R$ ${price}`}</p>
-        </div>
-      </Link>
->>>>>>> ebc56a72f13558f6a6c068c0b421863db6439dc5
     );
   }
 }
