6fb5620039eac5aa0a43708f19262cd7e8ed2f8c
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-02-10 01:10:35 -0300

    estilizaçao das posiçoes da barra de pesquisa, barra de categorias e card de produtos

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../styles/CategoriesList.css';
 
 export default class CategoriesList extends Component {
   click = () => {
     const { handleClick, idCategory } = this.props;
-    console.log(idCategory);
     handleClick(idCategory);
   }
 
   render() {
     const { categorieName } = this.props;
     return (
-      <div>
+      <div className="categories-buttons-container">
         <button
           onClick={ this.click }
           type="button"
           data-testid="category"
+          className="categorie-button"
         >
           {categorieName}
         </button>
